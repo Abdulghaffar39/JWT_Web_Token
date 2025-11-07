4e601d7874dd1297d7f8e8602aa41ee8f0755cd6
abdulghaffar39 <abdulghaffar15a@gmail.com>
2025-11-07 09:49:33 +0500

    working

--- public/app.js
+++ public/app.js
@@ -1,1 +1,35 @@
 
+function signup(e) {
+
+    e.preventDefault();
+
+    let fname = document.getElementById("firstName").value
+    let lname = document.getElementById("lastName").value
+    let email = document.getElementById("email").value
+    let password = document.getElementById("password").value
+    let role = document.getElementById("role")
+
+    console.log('testing', fname, lname, email, password);
+    
+
+    if (fname === "" || lname === "" || email === "" || password === "") {
+
+        alert('Please fill in all fields!');
+        return;
+    }
+    
+
+    if ( role.option.length === 0 ) {
+
+        alert('Role is empty');
+        return;
+    }else{
+        console.log('working successfuly');
+        
+    }
+
+
+    window.location.href = 'login.html'
+
+}
+
